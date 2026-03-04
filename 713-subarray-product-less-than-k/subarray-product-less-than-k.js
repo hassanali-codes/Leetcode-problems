315ef969d8fe4ab8b78daf75b32a40839ac11705
hassanali-codes <ha7298624@gmail.com>
2026-03-05 01:10:16 +0500

    Time: 4 ms (79.50%) | Memory: 61.1 MB (38.72%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.js
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.js
@@ -4,20 +4,20 @@
  * @return {number}
  */
 var numSubarrayProductLessThanK = function(nums, k) {
-    if(k <= 1) return 0;
-    
+    if(k == 0 || k ==  1) return 0;
+
     let left = 0;
-    let count = 0;
     let product = 1;
+    let count = 0;
 
     for(let right = 0; right < nums.length; right++){
         product *= nums[right];
-        
+
         while(product >= k){
             product /= nums[left];
-            left++;
-        }
-        count += (right - left + 1);
+            left++
+    }
+    count += (right - left + 1)
     }
     return count;
 }
