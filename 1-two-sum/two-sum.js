131ba120f26a2a3fff64fa17788a86d54c937aee
hassanali-codes <ha7298624@gmail.com>
2026-03-25 16:55:54 +0500

    Time: 2 ms (68.21%) | Memory: 56.8 MB (11.94%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -5,14 +5,15 @@
  */
 
 function twoSum(arr, target){
-    for (let i = 0; i < arr.length; i++){
-        for (let j = i + 1; j < arr.length; j++){
-            if (arr[i] + arr[j] === target){
-                return [i, j];
-            }
+    let map = new Map();
+
+    for(let i = 0; i < arr.length; i++){
+        let complement = target - arr[i]
+
+        if(map.has(complement)){
+            return [map.get(complement), i]
         }
+        map.set(arr[i], i)
     }
+    return []
 }
-const arr = [2, 7, 11, 15];
-const target = 9;
-console.log(twoSum(arr, target));
