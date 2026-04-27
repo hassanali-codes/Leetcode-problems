8772656c6ba57cbdcd589ca2a0e9c72109262bb7
hassanali-codes <ha7298624@gmail.com>
2026-04-27 20:01:27 +0500

    Time: 2 ms (76.34%) | Memory: 63.7 MB (15.94%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.js
+++ 11-container-with-most-water/container-with-most-water.js
@@ -15,3 +15,21 @@ var maxArea = function(height) {
     }
     return maxWater;
 }
+
+// var maxArea = function(height) {
+//     let maxWater = 0;
+//     let left = 0;
+//     let right = height.length - 1;
+
+//     while(left < right){
+//         let width = right - left;
+//         let h = Math.min(height[left], height[right])
+//         let currWater = width * h;
+//         maxWater = Math.max(maxWater, currWater)
+
+//     }
+//     return maxWater;
+
+// }
+
+
