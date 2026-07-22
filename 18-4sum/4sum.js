b54f64c0df0b95ab290ca3895312c9f77d9761ea
hassanali-codes <ha7298624@gmail.com>
2026-07-22 22:17:25 +0500

    Time: 15 ms (70.66%) | Memory: 57 MB (79.13%) - LeetSync

---
+++ 18-4sum/4sum.js
@@ -0,0 +1,66 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+var fourSum = function(nums, target) {
+    let result = []
+
+    nums.sort((a, b) => a-b)
+    for(let i = 0; i < nums.length-3; i++) {
+        if(i>0 && nums[i] === nums[i - 1])continue
+        for(let j = i + 1; j < nums.length - 2; j++) {
+            if(j> i + 1 && nums[j] === nums[j - 1])continue
+            let left = j + 1
+            let right = nums.length-1
+
+            while(left<right){
+                let sum = 
+                nums[i]+
+                nums[j]+
+                nums[left]+
+                nums[right];
+
+                if(sum < target)left++
+                else if(sum > target)right--
+                else{
+                    result.push([nums[i], nums[j], nums[left], nums[right]])
+ 
+                    while(left < right && nums[left] === nums[left+1])left++
+                    while(left < right && nums[right] === nums[right - 1])right--
+                    left++
+                    right--
+                }
+
+            }
+        }
+    }
+    return result
+};
+
+// ---------BRUTE FORCE---------
+// let set = new Set();
+//     for(let i = 0; i < nums.length; i++) {
+//         for(let j = i + 1; j < nums.length; j++) {
+//             for(let k = j + 1; k < nums.length; k++) {
+//                 for(let l = k + 1; l < nums.length; l++) {
+//                     let sum =
+//                         nums[i] +
+//                         nums[j] +
+//                         nums[k] +
+//                         nums[l];
+//                     if(sum === target) {
+//                         let quad = [
+//                             nums[i],
+//                             nums[j],
+//                             nums[k],
+//                             nums[l]
+//                         ];
+//                         quad.sort((a,b) => a-b);
+//                         set.add(JSON.stringify(quad));
+//                     }
+//                 }
+//             }
+//         }
+//     }
+//     return [...set].map(item => JSON.parse(item));
