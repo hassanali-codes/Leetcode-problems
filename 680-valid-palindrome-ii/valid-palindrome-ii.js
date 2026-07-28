82c19ebab8993d3c89a6baaea95fbee9ece9aa65
hassanali-codes <ha7298624@gmail.com>
2026-07-28 11:37:02 +0500

    Time: 7 ms (59.56%) | Memory: 61.2 MB (38.66%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var validPalindrome = function(s) {
+    let left = 0
+    let right = s.length-1
+
+    while(left < right){
+        if(s[left] === s[right]){
+            left++;
+            right--
+        }
+        else
+        return(
+            checkPalindrome(s, left + 1, right) ||
+            checkPalindrome(s, left, right - 1)
+        )
+    }
+    return true
+}
+
+function checkPalindrome(s, left, right){
+    while(left < right){
+        if(s[left] !== s[right]){
+            return false
+        }
+        left++
+        right--
+    }
+    return true
+}
