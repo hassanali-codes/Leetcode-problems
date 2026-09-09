6523913d0fb9c60a35454a28e5ad9259a2570364
hassanali-codes <ha7298624@gmail.com>
2026-09-09 17:10:20 +0500

    Time: 4 ms (45.95%) | Memory: 57.5 MB (47.97%) - LeetSync

---
+++ 841-shortest-distance-to-a-character/shortest-distance-to-a-character.js
@@ -0,0 +1,53 @@
+/**
+ * @param {string} s
+ * @param {character} c
+ * @return {number[]}
+ */
+var shortestToChar = function(s, c) {
+    let ans = new Array(s.length)
+
+    let pos = Infinity
+
+    for(let i=0; i < s.length; i++){
+        if(s[i]==c){
+            pos = i
+        }
+        ans[i] = Math.abs(i-pos)
+    }
+    pos = Infinity
+
+    for(let i=s.length-1; i>=0; i--){
+        if(s[i]==c){
+            pos = i
+        }
+        ans[i] = Math.min(ans[i], Math.abs(i-pos))
+    }
+    return ans
+}
+
+
+
+
+
+
+
+
+
+
+//------BRUTE FORCE--------
+// let ans = [];
+
+//     for (let i = 0; i < s.length; i++) {
+//         let min = Infinity;
+
+//         for (let j = 0; j < s.length; j++) {
+//             if (s[j] === c) {
+//                 min = Math.min(min, Math.abs(i - j));
+//             }
+//         }
+
+//         ans.push(min);
+//     }
+
+//     return ans;
+// };
