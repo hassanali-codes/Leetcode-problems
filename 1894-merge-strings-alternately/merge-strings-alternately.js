4c20416d52aba05be99489caec0162b75cc7b4ad
hassanali-codes <ha7298624@gmail.com>
2026-09-20 23:47:03 +0500

    Time: 47 ms (42.81%) | Memory: 54.6 MB (16.66%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.js
+++ 1894-merge-strings-alternately/merge-strings-alternately.js
@@ -4,40 +4,14 @@
  * @return {string}
  */
 var mergeAlternately = function(word1, word2) {
-    let merged = "";
-
+    let result = "";
     let i = 0;
-    let j = 0;
 
-    while (i < word1.length || j < word2.length) {
-
-        if(i < word1.length){
-            merged += word1[i];
-            i++;
-        }
-        if(j < word2.length){
-            merged += word2[j];
-            j++;
-        }
+    while (i < word1.length || i < word2.length) {
+        if (i < word1.length) result += word1[i];
+        if (i < word2.length) result += word2[i];
+        i++;
     }
-    return merged;
-}
 
-//----------BRUTE FORCE----------
-//     let merged = ""
-
-//     for(let i = 0; i < Math.min(word1.length, word2.length); i++){
-//         merged += word1[i]
-//         merged += word2[i]
-//     }
-//     let minLength = Math.min(word1.length, word2.length);
-
-//     for(let i = minLength; i < word1.length; i++){
-//         merged += word1[i]
-//     }
-
-//     for(let i = minLength; i < word2.length; i++){
-//         merged += word2[i]
-//     }
-//     return merged
-// }
+    return result;
+};
